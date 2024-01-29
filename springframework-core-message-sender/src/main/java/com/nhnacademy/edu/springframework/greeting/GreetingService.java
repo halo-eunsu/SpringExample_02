67bd8b09e2dddbd64be552106b0737fa8ae0b25f
halo-eunsu <ujd017@naver.com>
2024-01-29 17:04:13 +0900

    1

--- springframework-core-message-sender/src/main/java/com/nhnacademy/edu/springframework/greeting/GreetingService.java
+++ springframework-core-message-sender/src/main/java/com/nhnacademy/edu/springframework/greeting/GreetingService.java
@@ -8,7 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 public class GreetingService {
 
-    private final Greeter greeter;
+    private Greeter greeter;
+
+    public  GreetingService(){};
 
     public GreetingService(Greeter greeter) {
         this.greeter = greeter;
@@ -17,5 +19,9 @@ public class GreetingService {
     public boolean greet() {
         // 인터페이스의 메소드를 호출하지만 실제 구현 객체의 메소드가 실행됩니다.
         return greeter.sayHello();
+    }
+
+    public void setGreeter(Greeter greeter) {
+        this.greeter = greeter;
     }
 }
