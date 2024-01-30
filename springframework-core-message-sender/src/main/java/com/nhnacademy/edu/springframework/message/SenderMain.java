12e17dabb128192d67464ddf7fb53b9b413cbffc
halo-eunsu <ujd017@naver.com>
2024-01-30 13:15:17 +0900

    commit

--- springframework-core-message-sender/src/main/java/com/nhnacademy/edu/springframework/message/SenderMain.java
+++ springframework-core-message-sender/src/main/java/com/nhnacademy/edu/springframework/message/SenderMain.java
@@ -7,6 +7,7 @@ public class SenderMain {
 
     public static void main(String[] args) {
 
+
 //            User user =
 //        otry (ClassPathXmlApplicatinContext context = new ClassPathXmlApplicationContext("beans.xml")) {
 //            MessageSendService service = context.getBean("messageSender", MessageSendService.class);
