2a021c342c507bd06da091223b17ae27757f856f
halo-eunsu <ujd017@naver.com>
2024-01-30 10:09:53 +0900

    1

--- springframework-core-message-sender/src/main/java/com/nhnacademy/edu/springframework/Main.java
+++ springframework-core-message-sender/src/main/java/com/nhnacademy/edu/springframework/Main.java
@@ -1,7 +1,21 @@
 package com.nhnacademy.edu.springframework;
 
+import com.nhnacademy.edu.springframework.greeting.GreetingService;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+        try (ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("beans.xml")) {
+            GreetingService service = context.getBean("greetingService", GreetingService.class);
+            service.greet();
+        }
+
     }
+
+
+
+
+
 }
