380122e3dab43fbeb3e37b86216dfd83fbe67a9d
halo-eunsu <ujd017@naver.com>
2024-01-30 14:59:16 +0900

    commit

--- springframework-core-message-sender/src/main/java/com/nhnacademy/edu/springframework/component/FactoryMethodComponent.java
+++ springframework-core-message-sender/src/main/java/com/nhnacademy/edu/springframework/component/FactoryMethodComponent.java
@@ -1,4 +1,19 @@
 package com.nhnacademy.edu.springframework.component;
 
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+@Component
 public class FactoryMethodComponent {
+
+    @Bean
+    @Qualifier("public")
+    public TestBean publicInstance() {
+        return new TestBean("publicInstance");
+    }
+
+    public void doWork() {
+        // Component method implementation omitted
+    }
 }
