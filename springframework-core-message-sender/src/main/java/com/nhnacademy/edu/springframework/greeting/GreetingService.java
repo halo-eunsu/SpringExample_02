0064cc6703884387bab172420af2fa2f78033e63
halo-eunsu <ujd017@naver.com>
2024-01-30 11:12:03 +0900

    @SMS example commit

--- springframework-core-message-sender/src/main/java/com/nhnacademy/edu/springframework/greeting/GreetingService.java
+++ springframework-core-message-sender/src/main/java/com/nhnacademy/edu/springframework/greeting/GreetingService.java
@@ -5,24 +5,28 @@ package com.nhnacademy.edu.springframework.greeting;
 //import com.nhnacademy.edu.springframework.greeting.stereotype.Language;
 import com.nhnacademy.edu.springframework.Greeter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Required;
 import org.springframework.beans.factory.annotation.Value;
 
 public class GreetingService {
 
     @Value("${from}")
+    private String from;
 
     private Greeter greeter;
-    private final String name;
+    //private final String name;
 
     public  GreetingService(){};
-    public GreetingService(Greeter greeter) {
+    public GreetingService(@Qualifier("koreanGreeter") Greeter greeter) {
 
         this.greeter = greeter;
+
     }
 
     public boolean greet() {
         // 인터페이스의 메소드를 호출하지만 실제 구현 객체의 메소드가 실행됩니다.
+        System.out.println("From : " + from);
         return greeter.sayHello();
     }
 
