3a944f22d01070f21281040e9d50a698582439f9
halo-eunsu <ujd017@naver.com>
2024-01-31 13:26:29 +0900

    commit

---
+++ springframework-core-message-sender/src/main/java/com/nhnacademy/edu/springframework/aspectj/ElaspedAspect.java
@@ -0,0 +1,34 @@
+package com.nhnacademy.edu.springframework.aspectj;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StopWatch;
+
+
+@Aspect
+@Component
+public class ElaspedAspect {
+
+
+@Pointcut("execution(* com.nhnacademy.edu.springframework.greeting.service.Greeter.sayHello()")
+    public void targetGreeter(){}
+
+    @Around("targetGreeter()")
+    public Object doBasicProfiling(ProceedingJoinPoint pjp) throws  Throwable{
+
+        StopWatch
+
+    }
+
+
+}
+
+
+
+//public  interface Greeter{
+
+    boolean sayHello();
+}
---
+++ springframework-core-message-sender/src/main/java/com/nhnacademy/edu/springframework/aspectj/Timelogging.java
@@ -0,0 +1,14 @@
+package com.nhnacademy.edu.springframework.aspectj;
+
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+
+@Aspect
+@Component
+public class Timelogging {
+
+    @Pointcut("execution(* com.nhnacademy.edu.springframework.message.*")
+}
